274eb2f9949fa76f5bb0c18ff2565d15de36ffec
Yahya Saddiq <yahya.saddiq1@vodafone.com>
2019-10-09 03:19:04 +0200

    Initial Commit

---
+++ YSLoader/YSLoader.h
@@ -0,0 +1,19 @@
+//
+//  YSLoader.h
+//  YSLoader
+//
+//  Created by Yahya Saddiq on 10/9/19.
+//  Copyright © 2019 Yahya Saddiq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YSLoader.
+FOUNDATION_EXPORT double YSLoaderVersionNumber;
+
+//! Project version string for YSLoader.
+FOUNDATION_EXPORT const unsigned char YSLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSLoader/PublicHeader.h>
+
+
