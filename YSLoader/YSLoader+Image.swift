f3241e0ca8f8cfd607e790c1d01f48303d091a73
Yahya Saddiq <yahya.saddiq1@vodafone.com>
2019-10-13 08:22:59 +0200

    guard for duplicate image requests

--- YSLoader/YSLoader+Image.swift
+++ YSLoader/YSLoader+Image.swift
@@ -12,6 +12,15 @@ import AlamofireImage
 
 extension YSLoader {
     internal func loadImage(with url: String, completionHandler: @escaping Handler<UIImage>) {
+        // guards for duplicate requests
+        Alamofire.SessionManager.default.session.getAllTasks { (tasks) in
+            if tasks.firstIndex(where: { $0.originalRequest?.url?.absoluteString == url }) == nil {
+                print("skip download this image \(url)")
+                return
+            }
+        }
+
+        // return image if cached
         if let image = imageCache.image(withIdentifier: url) {
             completionHandler(.success(image))
             return
@@ -31,6 +40,7 @@ extension YSLoader {
                         }
                         return
                 }
+                // cache the image
                 self?.imageCache.add(image, withIdentifier: url)
                 completionHandler(.success(image))
         }
